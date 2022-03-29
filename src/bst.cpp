a9b5b0ad3fee829400dd6e36180446f337758fdc
alient12 <ali_ent12@outlook.com>
2022-03-29 22:39:23 +0430

    Node constructors

--- src/bst.cpp
+++ src/bst.cpp
@@ -1,1 +1,22 @@
 #include "bst.h"
+
+BST::Node::Node(int value, Node* left, Node* right)
+{
+    this->value = value;
+    this->left = left;
+    this->right = right;
+}
+
+BST::Node::Node()
+{
+    this->value = 0;
+    this->left = nullptr;
+    this->right = nullptr;
+}
+
+BST::Node::Node(const Node& node)
+{
+    this->value = node.value;
+    this->left = node.left;
+    this->right = node.right;
+}
