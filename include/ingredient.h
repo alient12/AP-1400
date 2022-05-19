389cf14d5d8909cb218140b690eac23d9438cf04
--global <ali_ent12@outlook.com>
2022-05-19 13:49:17 +0430

    ingredient class

--- include/ingredient.h
+++ include/ingredient.h
@@ -1,4 +1,22 @@
 #ifndef INGREDIENT_H
 #define INGREDIENT_H
 
+class Ingredient
+{
+public:
+    double get_price_unit(){return price_unit;};
+    size_t get_units(){retrun units;};
+    virtual std::string get_name() = 0;
+
+    double price(){return price_unit * units;};
+    
+
+protected:
+    Ingredient(double price_unit, size_t units): price_unit(price_unit), units(units){};
+
+    double price_unit;
+    size_t units;
+    std::string name;
+};
+
 #endif // INGREDIENT_H
